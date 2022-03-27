2d8469ce8ac7b52a37a5cf26cf271d50d07c2595
Lawrence <lawrencemurry@yahoo.com>
2022-03-27 09:45:36 +0000

    Uncomment code in production.rb that was throwing aws s3 signature error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,29 @@
 Rails.application.configure do
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
-    :s3_credentials => {
-      :bucket => ENV['AWS_S3_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :s3_region => ENV['AWS_REGION']
-    }
+  # devise says to define default url
+  config.action_mailer.default_url_options = { :host => 'secure.simple-milia-app.com', :protocol => 'https' }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
   }
+  
+  # config.paperclip_defaults = {
+  #   :storage => :s3,
+  #   s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+  #   :s3_credentials => {
+  #     :bucket => ENV['AWS_S3_BUCKET'],
+  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+  #     :s3_region => ENV['AWS_REGION']
+  #   }
+  # }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -92,15 +106,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  ActionMailer::Base.delivery_method = :smtp
-  host = 'murry-saas-app.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
-  }
+  # ActionMailer::Base.delivery_method = :smtp
+  # host = 'murry-saas-app.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com'
+  # }
 end
